3b45a0e0c2327d35345f5ec02ff8ce0f40476840
lvshijie <lvshijie@aiwending.com>
2018-12-26 11:01:59 +0800

    up

--- test/test.js
+++ test/test.js
@@ -41,7 +41,7 @@ describe('#math',() => {
       expect(cover(3,3)).to.be.equal(6)
     })
     it('should return 12 when cover(6,6)',()=> {
-      expect(cover(3,3)).to.be.equal(12)
+      expect(cover(6,6)).to.be.equal(12)
     })
   })
 })
